7b2cb5531efc2e3070a92790fedf4a7a6d41240b
WWPHUZTR\LENOVO <wwphuztr@gmail.com>
2024-06-08 08:13:01 +0700

    first commit

---
+++ tests/login.test.ts
@@ -0,0 +1,24 @@
+import test, { Page, chromium } from "@playwright/test";
+
+
+test.beforeAll(async ({}, testInfo) => {
+    
+});
+  
+test.only("Login test demo", async ({page, context}) => {
+
+    await page.goto("https://beta.hideout.cyberplusplus.io/");
+    await page.getByPlaceholder(' ').fill('wwtest@mailinator.com');
+    await page.getByRole('button', { name: 'Login' }).click();
+    await page.waitForLoadState('networkidle');
+
+    const page2 = await context.newPage();
+    await page2.bringToFront();
+    await page2.goto("https://www.mailinator.com/v4/public/inboxes.jsp");
+    await page2.waitForLoadState('networkidle')
+
+})
+
+test.afterAll(async ({browser}, testInfo) => {
+    
+});
