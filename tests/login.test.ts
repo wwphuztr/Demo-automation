db5631eff12f12ea1e66267f3588361071b0a3ce
WWPHUZTR\LENOVO <wwphuztr@gmail.com>
2024-06-08 17:55:10 +0700

    no message

--- tests/login.test.ts
+++ tests/login.test.ts
@@ -5,7 +5,35 @@ import test, { Page, chromium } from "@playwright/test";
     
 // });
   
-test("Login test demo", async ({page, context}) => {
+test("Login test demo", async ({page, context, browser}) => {
+
+    await page.goto("https://beta.hideout.cyberplusplus.io/");
+    await page.getByPlaceholder(' ').fill('wwtest@mailinator.com');
+    await page.getByRole('button', { name: 'Login' }).click();
+    await page.waitForLoadState('networkidle');
+
+    const page2 = await context.newPage();
+    await page2.bringToFront();
+    await page2.goto("https://www.mailinator.com/v4/public/inboxes.jsp");
+    await page2.waitForLoadState('networkidle')
+
+})
+
+test("Login test demo2", async ({page, context, browser}) => {
+
+    await page.goto("https://beta.hideout.cyberplusplus.io/");
+    await page.getByPlaceholder(' ').fill('wwtest@mailinator.com');
+    await page.getByRole('button', { name: 'Login' }).click();
+    await page.waitForLoadState('networkidle');
+
+    const page2 = await context.newPage();
+    await page2.bringToFront();
+    await page2.goto("https://www.mailinator.com/v4/public/inboxes.jsp");
+    await page2.waitForLoadState('networkidle')
+
+})
+
+test("Login test demo3", async ({page, context, browser}) => {
 
     await page.goto("https://beta.hideout.cyberplusplus.io/");
     await page.getByPlaceholder(' ').fill('wwtest@mailinator.com');
