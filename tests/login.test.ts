36f42645d856bf878fc577fea0fe5573397d17b7
WWPHUZTR\LENOVO <wwphuztr@gmail.com>
2024-06-09 22:38:33 +0700

    no message

--- tests/login.test.ts
+++ tests/login.test.ts
@@ -6,11 +6,12 @@ test.beforeAll(async ({}, testInfo) => {
 });
   
 test("Login test demo", async ({page, context, browser, isMobile}) => {
-    const desktop_email = "wwtest10@mailinator.com";
-    const mobile_email = "wwtest11@mailinator.com";
+    const desktop_email = "wwtest17@mailinator.com";
+    const mobile_email = "wwtest18@mailinator.com";
     await page.goto("https://beta.cyberplusplus.io/khoa-top-site");
     
     if(isMobile) {
+        await page.getByRole('button', { name: 'Accept all cookies' }).click();
         await page.locator("//input[@id='email']").fill(mobile_email);
     }
     else {
@@ -32,22 +33,26 @@ test("Login test demo", async ({page, context, browser, isMobile}) => {
     }
 
     await page2.getByRole('button', { name: 'GO' }).click();
-    // await page2.locator("((//*[*[normalize-space()='Cyber++'] and *[normalize-space()='22 min'] ])[last()]//*[normalize-space()='Cyber++'])[1]").click();
-
-    const element = page2.locator("((//*[*[normalize-space()='Cyber++'] and *[normalize-space()='just now'] ])[last()]//*[normalize-space()='Agilyte account security code'])[1]").click();
+    await page2.locator("((//*[*[normalize-space()='Cyber++'] and *[normalize-space()='just now'] ])[last()]//*[normalize-space()='Agilyte account security code'])[1]").click();
+    let otp;
 
     if(isMobile) {
-        const otp = await page2.frameLocator('iframe[name="html_msg_body"]').locator("//*[text()='10 minutes']/following::*[1]").textContent();
+        otp = await page2.frameLocator('iframe[name="html_msg_body"]').locator("//*[text()='10 minutes']/following::*[1]").textContent();
     }
     else {
-        const otp = await page2.locator("//*[text()='10 minutes']/following::*[1]").textContent();
+        otp = await page2.frameLocator('iframe[name="html_msg_body"]').locator("//*[text()='10 minutes']/following::*[1]").textContent();
     }
 
     await page.bringToFront();
     await page.locator("//input[@id='otp']").fill(otp);
     await page.locator("//button[text()='Verify']").click();
 
-    await expect(page.locator("//*[text()='OPEN TASK(s)']")).toHaveText('OPEN TASK(s)');
+    if (isMobile) {
+        await expect(page.locator("//*[name()='svg' and contains(@class, 'icon-tabler-bell')]")).toBeVisible();
+    }
+    else {
+        await expect(page.locator("//*[text()='OPEN TASK(s)']")).toHaveText('OPEN TASK(s)');
+    }
 })
 
 // test.afterAll(async ({browser}, testInfo) => {
