beec1956ef30453a6dd9d61ec81c43ebbea294bd
WWPHUZTR\LENOVO <wwphuztr@gmail.com>
2024-06-09 16:19:07 +0700

    no message

--- tests/login.test.ts
+++ tests/login.test.ts
@@ -1,32 +1,53 @@
-import test, { Page, chromium } from "@playwright/test";
+import test, { Page, chromium, expect } from "@playwright/test";
+import { log } from "console";
 
 
-// test.beforeAll(async ({}, testInfo) => {
-    
-// });
+test.beforeAll(async ({}, testInfo) => {
+});
   
-test("Login test demo", async ({page, context, browser}) => {
+test("Login test demo", async ({page, context, browser, isMobile}) => {
+    const desktop_email = "wwtest10@mailinator.com";
+    const mobile_email = "wwtest11@mailinator.com";
+    await page.goto("https://beta.cyberplusplus.io/khoa-top-site");
+    
+    if(isMobile) {
+        await page.locator("//input[@id='email']").fill(mobile_email);
+    }
+    else {
+        await page.locator("//input[@id='email']").fill(desktop_email);
+    }
 
-    await page.goto("https://beta.hideout.cyberplusplus.io/");
-    await page.getByPlaceholder(' ').fill('wwtest@mailinator.com');
-    await page.getByRole('button', { name: 'Login' }).click();
+    await page.locator("//button[text()='Sign In']").click();
     await page.waitForLoadState('networkidle');
 
     const page2 = await context.newPage();
     await page2.bringToFront();
     await page2.goto("https://www.mailinator.com/v4/public/inboxes.jsp");
-})
+    
+    if(isMobile) {
+        await page2.getByLabel('inbox field').fill(mobile_email);
+    }
+    else {
+        await page2.getByLabel('inbox field').fill(desktop_email);
+    }
 
-test("Navigate URL", async ({page, context, browser}) => {
+    await page2.getByRole('button', { name: 'GO' }).click();
+    // await page2.locator("((//*[*[normalize-space()='Cyber++'] and *[normalize-space()='22 min'] ])[last()]//*[normalize-space()='Cyber++'])[1]").click();
 
-    await page.goto("https://chatgpt.com/");
-    await page.waitForLoadState('networkidle');
-})
+    const element = page2.locator("((//*[*[normalize-space()='Cyber++'] and *[normalize-space()='just now'] ])[last()]//*[normalize-space()='Agilyte account security code'])[1]").click();
 
-test("Navigate URL2", async ({page, context, browser}) => {
+    if(isMobile) {
+        const otp = await page2.frameLocator('iframe[name="html_msg_body"]').locator("//*[text()='10 minutes']/following::*[1]").textContent();
+    }
+    else {
+        const otp = await page2.locator("//*[text()='10 minutes']/following::*[1]").textContent();
+    }
 
-    await page.goto("https://ngrok.com/download");
-    await page.waitForLoadState('networkidle');
+    await page.bringToFront();
+    await page.locator("//input[@id='otp']").fill(otp);
+    await page.locator("//button[text()='Verify']").click();
+
+    await expect(page.locator("//*[text()='OPEN TASK(s)']")).toHaveText('OPEN TASK(s)');
 })
 
 // test.afterAll(async ({browser}, testInfo) => {
